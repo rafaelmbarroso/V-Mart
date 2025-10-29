5afdba3e66429358fd8ab051785256e3202418d4
Ryan4890 <ryan.birge01@utrgv.edu>
2025-10-28 22:14:38 -0500

    database_started

--- market_App/models.py
+++ market_App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
